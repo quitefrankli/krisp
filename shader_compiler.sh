8d233398527a27605317408486ae17e3389f9ddc
quitefrankli <quitefrankli@hotmail.com>
2023-04-03 04:24:17 +1000

    [FEATURE] Add raytracing shaders

--- shader_compiler.sh
+++ shader_compiler.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
-set -e 
+set -e
 set -o pipefail
 
 SRC_DIR=$(dirname -- "${BASH_SOURCE[0]}")
 
 # only necessary for windows
 SRC_DIR=$(echo $SRC_DIR | sed 's/\\/\//g')
+OUTPUT_DIR=$SRC_DIR/build/shaders
+SHADERS_DIR=$SRC_DIR/src/shaders
 
-compile_dir()
+compile_if_file_exists()
 {
-	shader=$1
-	src_dir=$SRC_DIR/src/shaders/$shader
-	build_dir=$SRC_DIR/build/shaders/$shader
+	arguments=$1
+	shader_file_name=$2
+	shader_dir=$3
+	build_dir=$4
 
-	echo "compiling [$shader] shaders in [$src_dir] to [$build_dir]"
+	if [ -f $shader_dir/$shader_file_name.glsl ]
+	then
+		glslc $arguments $shader_dir/$shader_file_name.glsl -o $build_dir/$shader_file_name.spv
+	fi
+}
+
+compile_shader_dir()
+{
+	shader_name=$1
+	src_dir=$2
+	build_dir=$OUTPUT_DIR/$shader_name
+
+	echo "compiling [$shader_name] shaders from [$src_dir] to [$build_dir]"
 
 	mkdir -p $build_dir
 
-	glslc -fshader-stage=vertex $src_dir/vertex_shader.glsl -o $build_dir/vertex_shader.spv
-	glslc -fshader-stage=fragment $src_dir/fragment_shader.glsl -o $build_dir/fragment_shader.spv
+	compile_if_file_exists "-fshader-stage=vertex" vertex_shader $src_dir $build_dir
+	compile_if_file_exists "-fshader-stage=fragment" fragment_shader $src_dir $build_dir
+	compile_if_file_exists "-fshader-stage=rgen --target-env=vulkan1.2" raygen_shader $src_dir $build_dir
+	compile_if_file_exists "-fshader-stage=rchit --target-env=vulkan1.2" rayhit_shader $src_dir $build_dir
+	compile_if_file_exists "-fshader-stage=rmiss --target-env=vulkan1.2" raymiss_shader $src_dir $build_dir
 }
 
 #Black        0;30     Dark Gray     1;30
@@ -36,15 +54,15 @@ GREEN='\033[0;32m' # green
 NC='\033[0m' # No Color
 
 echo -e "${YELLOW}compiling shaders...${NC}"
-
-# for each folder in under shaders except "library" compile each folder
-for directory in $SRC_DIR/src/shaders/*/
+# for each folder in under $RASTERIZATION_SHADERS_DIR except "library" compile each folder
+for directory in $SHADERS_DIR/*/*/
 do
-	directory=$(basename $directory)
-	if [ $directory == library ]
+	shader_name=$(basename $directory)
+	if [ $shader_name == library ]
 	then
 		continue
 	fi
-	compile_dir $directory
+	compile_shader_dir $shader_name $directory
 done
+
 echo -e "${GREEN}shader compilation complete!${NC}"
